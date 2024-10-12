59f6da14610ba31c5442732c52442f85c563fc14
yjgaia <yjdev88@gmail.com>
2024-10-12 14:50:37 +0900

    Refactor chat module to add ChatMessageForm component

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 export { default as Author } from "./author/Author.js";
 export { default as ChatMessageGroup } from "./chat/ChatMessageGroup.js";
 export { default as ChatMessageList } from "./chat/ChatMessageList.js";
+export { default as ChatMessageForm } from "./chat/ChatMessageForm.js";
 export { default as SocialCompConfig } from "./SocialCompConfig.js";
 //# sourceMappingURL=index.d.ts.map
