e16c8b0e8f12a71c1eeab2a5e5296ce29dfc3368
yjgaia <yjdev88@gmail.com>
2024-12-02 13:45:15 +0900

    Remove deprecated chat message exports from index files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export { default as ChatMessageForm } from "./chat/ChatMessageForm.js";
-export { default as ChatMessageGroup } from "./chat/ChatMessageGroup.js";
-export { default as ChatMessageList } from "./chat/ChatMessageList.js";
 export { default as LoggedInUserAvatarButton } from "./logged-in-user/LoggedInUserAvatarButton.js";
 export { default as SocialCompConfig } from "./SocialCompConfig.js";
 export { default as User } from "./user/User.js";
