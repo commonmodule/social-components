2035b7e6cf0aa4580c068e895503b4850b36b16b
yjgaia <yjdev88@gmail.com>
2025-01-13 13:51:04 +0900

    Add PostForm component with title and content inputs

--- lib/post/PostForm.d.ts
+++ lib/post/PostForm.d.ts
@@ -1,6 +1,12 @@
 import { DomNode } from "@common-module/app";
 import Post from "./Post.js";
 export default class PostForm extends DomNode {
+    private titleInput;
+    private contentInput;
     constructor(existingPost?: Post);
+    getData(): {
+        title: string;
+        content: string;
+    };
 }
 //# sourceMappingURL=PostForm.d.ts.map
--- src/post/PostForm.ts
+++ src/post/PostForm.ts
@@ -1,8 +1,31 @@
 import { DomNode } from "@common-module/app";
+import { Input } from "@common-module/app-components";
 import Post from "./Post.js";
 
 export default class PostForm extends DomNode {
+  private titleInput: Input;
+  private contentInput: Input;
+
   constructor(existingPost?: Post) {
-    super(".post-form", "Post form");
+    super(".post-form");
+
+    this.append(
+      this.titleInput = new Input({
+        label: "Title",
+        value: existingPost?.title,
+      }),
+      this.contentInput = new Input({
+        label: "Content",
+        value: existingPost?.content,
+        multiline: true,
+      }),
+    );
+  }
+
+  public getData(): { title: string; content: string } {
+    return {
+      title: this.titleInput.value,
+      content: this.contentInput.value,
+    };
   }
 }
