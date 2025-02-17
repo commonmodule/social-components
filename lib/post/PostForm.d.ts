d7e6e762818583cfbd2004507ba4e90119ec1997
yjgaia <yjdev88@gmail.com>
2025-02-17 11:18:45 +0900

    Remove PostForm class and related files, updating dependencies in package.json

--- lib/post/PostForm.d.ts
+++ lib/post/PostForm.d.ts
@@ -1,12 +1,1 @@
-import { DomNode } from "@common-module/app";
-import Post from "./Post.js";
-export default class PostForm extends DomNode {
-    private titleInput;
-    private contentInput;
-    constructor(existingPost?: Post);
-    getData(): {
-        title: string;
-        content: string;
-    };
-}
 //# sourceMappingURL=PostForm.d.ts.map
